52f53275e9f51376d1d96d5c341aee991c5b659d
Enzo <enzo@dssi.fr>
2023-10-16 14:43:49 +0300

    premiere fois

---
+++ test.py
@@ -0,0 +1,42 @@
+#nom = "Enzo";
+
+# nom = input("Quels es votre nom ?  je")
+# age = "330c"
+
+
+# try:
+#     age_prochain = int(age) + 1
+# except ValueError:
+#     print("ENTRER UN NOMBRE")
+# else:
+#   print("je sui "+str(age_prochain))  
+
+# mot_passe = ""
+
+# while not mot_passe == "ENZO":
+#     mot_passe = input("DOnner votre mot de passe ") 
+
+# n=0
+# while n<5:
+#     print("valeur "+ str(n))
+#     n = n +1
+
+# age = 0
+
+# while age == 0 :
+#     age_str = input("votre age svp ")
+#     try:
+#         age = int(age_str)
+#     except:
+#         print("l'age : c 'est en nombre ")
+
+name = ""
+while  name == "":
+    name = input("Donnez moi votre nom ? ... ") 
+
+
+
+
+
+
+
